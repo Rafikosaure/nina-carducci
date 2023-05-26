b1a62ed4cc405b5a2780f92abd5fcb79fd090c79
rafik <rafikbensadi@live.fr>
2023-05-26 17:52:27 +0200

    Bug de navigation dans la galerie: reprise du code pour améliorer le comportement du correctif effectué plus tôt -> résultat finalisé !

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -155,7 +155,9 @@
         }
       });
 
-      next = imagesCollection[index - 1];
+      next =
+        imagesCollection[index - 1] ||
+        imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     nextImage() {
@@ -192,7 +194,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index + 1];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
